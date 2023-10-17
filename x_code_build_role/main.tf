025958d0703da51749d294b09e9d1e72ceb675c2
EhabSaid <EhabSaid016@Gmail.com>
2023-10-17 21:24:12 +0200

    fix error

--- x_code_build_role/main.tf
+++ x_code_build_role/main.tf
@@ -17,11 +17,11 @@ resource "aws_iam_role" "codebuild_role_other_accounts" {
             "Service": "codebuild.amazonaws.com"
         },
         "Action": "sts:AssumeRole"
-        },
-        {
+    },
+    {
         "Effect": "Allow",
         "Principal": {
-            "AWS": "arn:aws:iam::${var.deployment_account_id}:role/x_codebuild-service-role-dev-2acc"
+            "AWS": "arn:aws:iam::${var.deployment_account_id}:role/${var.x_codebuild_role_name}"
         },
         "Action": "sts:AssumeRole"
     }]
